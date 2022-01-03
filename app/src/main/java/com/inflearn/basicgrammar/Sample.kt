f89882d239692f658f3298048b0721ac8865ba0a
koolunkle <gugi329@daum.net>
2022-01-03 18:20:08 +0900

    4. 조건식

--- app/src/main/java/com/inflearn/basicgrammar/Sample.kt
+++ app/src/main/java/com/inflearn/basicgrammar/Sample.kt
@@ -11,6 +11,43 @@ fun main() {
     println("is this true? ${1 == 0}")
     println("this is 2\$a")
 
+    checkNum(1)
+
+}
+
+//    4. 조건식
+
+fun maxBy(a: Int, b: Int): Int {
+    if (a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+
+fun maxBy2(a: Int, b: Int) = if (a > b) a else b
+
+fun checkNum(score: Int) {
+
+    when (score) {
+        0 -> println("this is 0")
+        1 -> println("this is 1")
+        2, 3 -> println("this is 2 or 3")
+    }
+
+    var b = when (score) {
+        1 -> 1
+        2 -> 2
+        else -> 3
+    }
+    println("b: ${b}")
+
+    when (score) {
+        in 90..100 -> println("You are genius")
+        in 10..80 -> println("not bad")
+        else -> println("okay")
+    }
+
 }
 
 //    1. 함수
