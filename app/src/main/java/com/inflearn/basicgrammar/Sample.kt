65038be4d2e8ca6595c321db38cbbb68d89e8cd0
koolunkle <gugi329@daum.net>
2022-01-03 18:11:54 +0900

    3. Template String

--- app/src/main/java/com/inflearn/basicgrammar/Sample.kt
+++ app/src/main/java/com/inflearn/basicgrammar/Sample.kt
@@ -1,8 +1,16 @@
 package com.inflearn.basicgrammar
 
 fun main() {
-    helloWorld()
-    println(add(4, 5))
+
+//    3. Template String
+
+    val name = "jungi"
+    val lastname = "Kim"
+    println("my name is ${name + lastname} I'm 26")
+
+    println("is this true? ${1 == 0}")
+    println("this is 2\$a")
+
 }
 
 //    1. 함수
