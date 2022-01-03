e3cddea5c6e5fbdf83f22081eb5e830dc70aae29
koolunkle <gugi329@daum.net>
2022-01-03 18:09:12 +0900

    2. 변수

--- app/src/main/java/com/inflearn/basicgrammar/Sample.kt
+++ app/src/main/java/com/inflearn/basicgrammar/Sample.kt
@@ -14,3 +14,19 @@ fun helloWorld() {
 fun add(a: Int, b: Int): Int {
     return a + b
 }
+
+//    2. 변수
+
+fun valAndVar() {
+
+    val a: Int = 10
+    var b: Int = 9
+    b = 100
+
+    val c = 100
+    var d = 100
+
+    var e: String
+    var name = "jungi"
+
+}
